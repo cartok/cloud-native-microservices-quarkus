6f7a054c66b494a0071784c1b00db05cc61ba7b2
Dennis Weiershaeuser <dennis.weiershaeuser@gmail.com>
2026-02-05 14:36:46 +0100

    section 3.17-3.18

--- smartbar-backoffice/src/main/java/de/cartok/quarkus/tutorial/backoffice/api/CategoriesApiImpl.java
+++ smartbar-backoffice/src/main/java/de/cartok/quarkus/tutorial/backoffice/api/CategoriesApiImpl.java
@@ -3,8 +3,10 @@ package de.cartok.quarkus.tutorial.backoffice.api;
 import java.util.List;
 
 import de.cartok.quarkus.tutorial.backoffice.api.model.Category;
+import io.smallrye.common.annotation.NonBlocking;
 import jakarta.ws.rs.core.Response;
 
+@NonBlocking
 public class CategoriesApiImpl implements CategoriesApi {
   @Override
   public Response deleteCategory(String categoryId) {
