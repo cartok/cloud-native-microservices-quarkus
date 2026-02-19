25fc6932211ca0e5f92ad610021e1d327768e4f0
Dennis Weiershaeuser <dennis.weiershaeuser@gmail.com>
2026-02-19 14:52:26 +0100

    wip get started with oicd, therefore comment out / disable mtls for now, add some configs, start to replicate first oicd lesson

--- backoffice/src/test/java/de/cartok/quarkus/tutorial/backoffice/api/CategoryResourceTest.java
+++ backoffice/src/test/java/de/cartok/quarkus/tutorial/backoffice/api/CategoryResourceTest.java
@@ -6,14 +6,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
-import io.quarkus.test.security.TestSecurity;
 
 @QuarkusTest
 class CategoryResourceTest {
 
-  // TODO: At least in this example /categories is defined to be only accessible by "admin" role. Should create an additional test which validates error case.
   @Test
-  @TestSecurity(user = "bob", roles = {"admin"})
+    //  @TestSecurity(user = "bob", roles = {"admin"})
   void getCategoriesAsAdminUser() {
     final var response = given()
       .when()
